465b61be128ece1962d998d96ba9224d9637025e
sam <sam.owen.consultant@springer.com>
2019-05-10 14:05:02 +0100

    add how to run tests in readme

--- test/connect_4_test.py
+++ test/connect_4_test.py
@@ -16,7 +16,3 @@ class GameTestSuit(unittest.TestCase):
             [' ', ' ', ' ', ' ', ' ', ' '],
             [' ', ' ', ' ', ' ', ' ', ' ']
         ])
-
-
-if __name__ == '__main__':
-    unittest.main()
